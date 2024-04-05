7e44db18cba4f02760deef4ec55099dc472a6a4d
lGnyte <grasu.ioan02@gmail.com>
2024-04-05 12:55:16 +0300

    added starting point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="p-10">
@@ -7,3 +5,13 @@ export default function Home() {
     </main>
   );
 }
+
+
+/**
+ * Orice ruta din app va fi un folder cu numele rutei si un fisier page.tsx
+ * Exemplu: src/app/login/page.tsx
+ * Fisierul trebuie sa exporte o functie default care returneaza elemente JSX
+ * Layoutul este definit in src/app/layout.tsx
+ * Daca vreti sa folositi un layout diferit pentru o anumita ruta, puteti crea un fisier layout.tsx in folderul rutei
+ * Altfel, se va folosi cel mai apropiat layout pe care il gaseste in ierarhia de foldere
+ */
