7021686becb8ab439c9a77ed0f38555e4eaaa612
cerbucristi <cristimadalincerbu@gmail.com>
2024-05-24 21:09:18 +0300

    small fix

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -6,7 +6,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "@/components/CheckoutForm/CheckoutForm";
 
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""
 );
 
 export default function Checkout() {
