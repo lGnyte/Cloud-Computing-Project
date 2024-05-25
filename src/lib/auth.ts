c83563fa5b525d14687d19cf600600d1dd1471de
lGnyte <grasu.ioan02@gmail.com>
2024-05-26 02:11:05 +0300

    added email and password authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import toast from "react-hot-toast";
 import { auth, googleAuthProvider } from "./firebase";
-import { signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 export const signInWithGoogle = async () => {
   signInWithPopup(auth, googleAuthProvider)
@@ -10,5 +10,36 @@ export const signInWithGoogle = async () => {
     })
     .catch(err => {
       toast.error(err.message);
-  });
+    });
 };
+
+export const signIn = async (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      toast.success("Authenticated with email and password!");
+    })
+    .catch(err => {
+      if (err.code === "auth/invalid-login-credentials") {
+        toast.error("Invalid credentials");
+      } else {
+        console.error(err.message);
+        toast.error("Login failed!");
+      }
+    });
+}
+
+export const registerUser = async (email: string, password: string) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      toast.success("Registered and authenticated with email and password!");
+    })
+    .catch(err => {
+      if (err.code === "auth/invalid-email") {
+        toast.error("Invalid email");
+      } else if (err.code === "auth/email-already-in-use") {
+        toast.error("Email already exists");
+      } else {
+        toast.error("Registration failed!");
+      }
+    });
+}
